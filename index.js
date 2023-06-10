5f2c06af6521727c74c7318438c92d53e4b80b0e
Enes Kavçakar <eneskavcakar34@gmail.com>
2023-06-10 17:44:30 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const app = require("express")();
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+	console.log("Example app listening on port 3000!");
+});
