bf01f1013868c11100b4cbf2675cd7ba97d6f325
Enes Kavçakar <enes@kavcakar.com.tr>
2023-06-18 17:03:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,16 +11,28 @@ app.get("/", (req, res) => {
 
 app.get("/cpu", (req, res) => {
   const durationInSeconds = 5;
+  performCpuIntensiveTask(durationInSeconds);
+
+  console.log('CPU-intensive process completed.');
+  res.send('CPU-intensive process completed.');
+});
+
+app.get("/cpu2/:duration", (req, res) => {
+  const durationInSeconds = parseInt(req.params.duration, 10);
+  performCpuIntensiveTask(durationInSeconds);
+
+  console.log('CPU-intensive process completed.');
+  res.send('CPU-intensive process completed.');
+});
+
+function performCpuIntensiveTask(durationInSeconds) {
   const endTime = Date.now() + durationInSeconds * 1000;
 
   while (Date.now() < endTime) {
     const randomString = crypto.randomBytes(1024).toString('hex');
     crypto.pbkdf2Sync(randomString, 'salt', 100000, 512, 'sha512');
   }
-
-  console.log('CPU-intensive process completed.');
-  res.send('CPU-intensive process completed.');
-});
+}
 
 app.listen(3000, () => {
   console.log("Example app listening on port 3000!");
