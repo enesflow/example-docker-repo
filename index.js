1fb4a34f0b88b5033a23b4bdbbfbcfa9f5389030
Enes Kavçakar <enes@kavcakar.com.tr>
2023-08-01 17:05:12 +0300

    add directory stuff

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const app = require("express")();
 const crypto = require('crypto');
+const fs = require('fs');
+const path = require('path');
 
 let visits = 0;
 
@@ -9,15 +11,7 @@ app.get("/", (req, res) => {
   res.send(`This page has been visited ${visits} times`);
 });
 
-app.get("/cpu", (req, res) => {
-  const durationInSeconds = 5;
-  performCpuIntensiveTask(durationInSeconds);
-
-  console.log('CPU-intensive process completed.');
-  res.send('CPU-intensive process completed.');
-});
-
-app.get("/cpu2/:duration", (req, res) => {
+app.get("/cpu/:duration", (req, res) => {
   const durationInSeconds = parseInt(req.params.duration, 10);
   performCpuIntensiveTask(durationInSeconds);
 
@@ -28,6 +22,30 @@ app.get("/cpu2/:duration", (req, res) => {
 app.get("/test", (req, res) => {
   const testValue = process.env.TEST;
   res.send(`The value of TEST environment variable is: ${testValue}`);
+});
+
+// New route to list files/folders in a directory
+app.get("/list", (req, res) => {
+  const directory = process.env.DIRECTORY; // Replace 'DIRECTORY' with the name of your environment variable
+  if (!directory) {
+    return res.status(400).send('The DIRECTORY environment variable is not set.');
+  }
+
+  fs.access(directory, fs.constants.F_OK, (err) => {
+    if (err) {
+      console.error(`Directory does not exist: ${directory}`);
+      return res.status(400).send('The specified directory does not exist.');
+    }
+
+    fs.readdir(directory, (err, files) => {
+      if (err) {
+        console.error(`Error reading directory: ${err.message}`);
+        return res.status(500).send('Error reading directory.');
+      }
+
+      res.send(`Files/Folders in ${directory}: ${files.join(', ')}`);
+    });
+  });
 });
 
 function performCpuIntensiveTask(durationInSeconds) {
